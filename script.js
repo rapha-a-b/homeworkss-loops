4d06676a7823f5cf7b761ee0ce5dffa219beaf10
rapha-a-b <119280461+rapha-a-b@users.noreply.github.com>
2023-02-21 06:06:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// // while()
+
+// // for
+
+// //for in
+
+// ///for of
+// // let users = [
+// //   { name: "bla", amount: 1000 },
+// //   { name: "bla", amount: 1000 },
+// // ];
+
+// let usersObject = [
+//   { name: "bla", amount: 1000 },
+//   { name: "or", amount: 1000 },
+// ];
+
+// for (let x of usersObject) {
+//   console.log(`${x.name} ${x.amount}`);
+// }
+
+// let moran = 0;
+// while (moran < 10) {
+//   console.log(moran);
+//   moran++;
+
+//   if (moran > 3) {
+//   }
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   if (i >= 2) {
+//     break;
+//   }
+//   console.log("Or has 1000000$");
+// }
+
+// let testOF = [{ a: "1", b: "2" }];
+// for (let x of testOF) {
+//   console.log(x.a + x.b);
+// }
+
+// let testIn = { a: "1", b: "2" };
+// for (let x in testIn) {
+//   console.log(x);
+// }
+
+let numLevles = prompt("how many levels?");
+let drawStars = "";
+
+// for (i = 0; i < numLevles; i++) {
+//   drawStars += "*";
+//   console.log(drawStars);
+// }
+
+// for (i = numLevles; i >= 0; i--) {
+//   drawStars = "";
+//   for (j = 0; j <= i; j++) {
+//     drawStars += "*";
+//   }
+//   console.log(drawStars);
+// }
+
+// let stars = [];
+// let drawStars2 = " ";
+// for (i = 0; i < numLevles; i++) {
+//   drawStars2 += "*";
+//   stars.push(drawStars2);
+//   // console.log(drawStars2);5
+// }
+
+// for (i = 0; i < numLevles; i++) {
+//   console.log(stars.pop());
+// }
+
+spaceCounter = Math.round(numLevles);
+for (i = 0; i < numLevles; i++) {
+  drawStars = "";
+  if (i % 2 === 0) {
+    if (spaceCounter > 0) {
+      for (k = spaceCounter; k > 0; k--) {
+        drawStars += " ";
+      }
+      spaceCounter--;
+    }
+    for (j = 0; j <= i; j++) {
+      drawStars += "*";
+    }
+    console.log(drawStars);
+  }
+}
